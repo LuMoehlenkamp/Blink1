0b9a84a95a6f5558091bccbbb69c1c908b7f6ce7
Ludger Möhlenkamp <ludger.moehlenkamp@googlemail.com>
2018-09-23 20:27:58 +0200

    serial function calls included

--- Blink1/main.cpp
+++ Blink1/main.cpp
@@ -2,20 +2,42 @@
 #include <wiringSerial.h>
 #include <termios.h>
 #include <debug/debug.h>
-#include <cstdio>
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
+
+#include <cstdio>
 
 #define	LED	17
 
 int main(void){
 	auto i = wiringPiSetupSys();
 	pinMode(LED, OUTPUT);
-	// stdout << i << std::endl;
+
 	int fd = 0;
+	if ((fd = serialOpen("/dev/ttyAMA0", 9600)) < 0){
+		fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
+		return 1;
+	}
+
 	termios options;
+  termios optionsReadOut;
 	tcgetattr(fd, &options);
 	options.c_cflag = B9600|CS7|PARENB|CSTOPB;
 	auto j = tcsetattr(fd, 0,&options);
+  tcgetattr(fd, &optionsReadOut);
+
+  /*for (int count = 0; count < 100; ++count) {
+    serialPutchar(fd, count);
+  }*/
+  serialPuts(fd, "MoinMoin");
+	
+	delay(3);
+  auto x = serialDataAvail(fd);
+	while (serialDataAvail(fd)){
+		printf(" -> %3d", serialGetchar(fd));
+		fflush(stdout);
+	}
 	
 	while (true){
 		digitalWrite(LED, HIGH);  // On
