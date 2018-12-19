920a78c7e8d11f8ab251e952b8e8984be1d2537f
Ludger Möhlenkamp <ludger.moehlenkamp@googlemail.com>
2018-12-20 00:12:36 +0100

    mqtt broker added

--- Blink1/main.cpp
+++ Blink1/main.cpp
@@ -9,23 +9,26 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <iostream>
-
 #include <cstdio>
 
 #include "serial.h"
+#include "mqttWrapper.h"
 
 #define	LED	0
 
 int main(void)
 {
   serial serialDevice;
-  
+  mqttWrapper *mqttHandler;
+  mqttHandler = new mqttWrapper("mqttTest","test","192.168.178.34", 1883);
+  mqttHandler->send_message("test");
   while (true) 
   {
     if (serialDevice.serialRead() > 0) 
     {
       // if new data is available on the serial port, print it out
       std::cout << serialDevice.getData();
+      mqttHandler->send_message("test");
     }
     else 
     {
